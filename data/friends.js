a6a801c3712c3714540d7b75075bb9086b5763e1
MMPark <parkmee@gmail.com>
2018-11-12 22:34:37 -0500

    populated seed data and formatted survey page

---
+++ data/friends.js
@@ -0,0 +1,17 @@
+const peopleArr = [
+    {
+        name: "Anne Rice",
+        photo: "https://hoodwork-production.s3.amazonaws.com/uploads/story/image/28671/Anne_Rice_hi-res.jpg",
+        scores: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name: "Jim Jeffries",
+        photo: "https://pbs.twimg.com/profile_images/1023913158916890630/gyVk0wOT_400x400.jpg",
+        scores: [5,3,4,1,2,5,3,4,4,2]
+    },
+    {
+        name: "Oprah Winfrey",
+        photo: "https://images.gr-assets.com/authors/1354837955p5/3518.jpg",
+        scores: [2,4,5,5,3,2,1,4,2,3]
+    }
+]
